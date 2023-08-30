b15b2b206b296b58f5268ae53a093bbae31c9fd1
Kibiko <kevinjunchan@gmail.com>
2023-08-30 10:22:04 +0100

    revised edition

--- script.js
+++ script.js
@@ -4,58 +4,52 @@ const list = document.querySelector("#list");
 const dateButton = document.querySelector("#date");
 const date = document.querySelector("#show-date");
 const completedList = document.querySelector("#completed-list");
-var myInput;
 
 const showDate = () => {
     console.log(Date());
-    date.innerText = Date();
+    dateButton.innerText = Date();
 }
 
 //date button 
 dateButton.addEventListener("click", showDate)
 
-
-//print method for button 
-const printButtonConfirmation = () => {
-    console.log("button clicked");
-};
-
-//text-input
-input.addEventListener("input", (event) => {
-    myInput = event.target.value;
-});
-
 //create and append list
 const createAndAppendListItem = () => {
     const newListItem = document.createElement("li");
-    newListItem.innerText = myInput;
-    list.appendChild(newListItem);
+    newListItem.innerText = input.value;
+    const newCompletedItem = document.createElement("li");
+    newCompletedItem.innerText = newListItem.innerText;
+
+    //delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click",() => {
+        list.removeChild(newListItem);
+    });
+    deleteButton.className = "delete";
+    newListItem.appendChild(deleteButton);
 
     //complete button
     const completeButton = document.createElement("input");
     completeButton.type = "checkbox";
-    const newCompletedItem = document.createElement("li");
+    const deleteComplete = document.createElement("button");
     completeButton.onclick = () => {
         if(completeButton.checked == true){
-            newCompletedItem.innerText = newListItem.innerText;
             newListItem.classList.add("completedTask");
             completedList.appendChild(newCompletedItem);
+            deleteComplete.addEventListener("click", () => {
+                completedList.removeChild(newCompletedItem);
+            })
+            deleteComplete.innerText = "Delete";
+            deleteComplete.className = "delete";
+            newCompletedItem.appendChild(deleteComplete);
         } else {
             newListItem.classList.remove("completedTask");
-            newCompletedItem.remove();
+            completedList.removeChild(newCompletedItem);
         }
     }
-    list.appendChild(completeButton);
-
-    //delete button
-    const deleteButton = document.createElement("button");
-    deleteButton.addEventListener("click",() => {
-        newListItem.remove();
-        deleteButton.remove();
-        completeButton.remove();
-    })
-    deleteButton.innerHTML = "Delete";
-    list.appendChild(deleteButton);
+    newListItem.appendChild(completeButton);
+    list.appendChild(newListItem);
 }
 
 //button click
